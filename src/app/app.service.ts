56a9fe9cfd2c8eb301d4deffac74bc151bbeb97b
Lovesh Gupta <loveshgupta13579@gmail.com>
2023-03-15 00:32:02 +0530

    env

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
+  env = environment.apiUrl;
   constructor(private http: HttpClient) {}
   contact(data: any) {
     return this.http.post('/api', data);
   }
   getData() {
-    return this.http.get('/api/read');
+    return this.http.get(this.env + '/read');
   }
 }
